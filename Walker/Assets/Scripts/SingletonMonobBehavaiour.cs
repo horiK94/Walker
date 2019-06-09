e0b950e88cedc3da928dffcf6d9e197fb443c163
horiK <hazemi94@gmail.com>
2019-06-09 20:14:50 +0900

    シングルトンクラスの作成

---
+++ Walker/Assets/Scripts/SingletonMonobBehavaiour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// シングルトンクラス
+/// https://qiita.com/okuhiiro/items/3d69c602b8538c04a479 参考
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);      //全ゲームオブジェクトを検索
+                if (instance == null)
+                {
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        // 他のGameObjectにアタッチされているか調べる.
+        // アタッチされている場合は破棄する.
+        if (this != Instance)
+        {
+            Destroy(this);
+            //Destroy(this.gameObject);
+            Debug.LogError(
+                typeof(T) +
+                " は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました." +
+                " アタッチされているGameObjectは " + Instance.gameObject.name + " です.");
+            return;
+        }
+
+        //DontDestroyOnLoad(this.gameObject);
+    }
+
+}
