48d7d2cda29bfafe6fcec83920ed9b67f6aeb4f2
horiK <hazemi94basic@gmail.com>
2020-05-05 01:36:02 +0900

    階数保存コンポーネントの実装

---
+++ Walker/Assets/Scripts/ScoreDataManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreDataManager : SingletonMonoBehaviour<ScoreDataManager>
+{
+    /// <summary>
+    /// 階数
+    /// </summary>
+    public int Floor { get; private set; } = 0;
+
+    /// <summary>
+    /// リセット
+    /// </summary>
+    public void Reset()
+    {
+        Floor = 0;
+    }
+
+    /// <summary>
+    /// 上の階に移動
+    /// </summary>
+    public void GoUpstairs()
+    {
+        Floor++;
+    }
+}
