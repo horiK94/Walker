bbd7e9d97b75a98ad9407498496c1675fcde2dbb
horiK <hazemi94basic@gmail.com>
2020-05-05 00:07:47 +0900

    探索者の仮実装のためシーンを修正

--- Walker/Assets/Scripts/PlayerMover.cs
+++ Walker/Assets/Scripts/PlayerMover.cs
@@ -70,6 +70,10 @@ public class PlayerMover : MonoBehaviour
     /// </summary>
     Rigidbody rigid = null;
 
+    [SerializeField]
+    private PurserMover purserMover = null;
+
+
     private void Awake()
     {
         mainCameraTranfrom = Camera.main.transform;
@@ -79,6 +83,8 @@ public class PlayerMover : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
 
         transform.LookAt(mainCameraTranfrom);
+
+        purserMover.StartMove(transform);
     }
 
     private void Update()
