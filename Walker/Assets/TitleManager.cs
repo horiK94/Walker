a1c783aaef5ec3becca6260d129c10276dd253bf
horiK <hazemi94basic@gmail.com>
2020-05-06 00:34:00 +0900

    スタート画面の動作実装

---
+++ Walker/Assets/TitleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField]
+    Button startButton = null;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(startGame);
+    }
+
+    private void startGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
