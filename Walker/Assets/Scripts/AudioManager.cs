0df1eb279cc3ec782ffe70df1da0e2750a707f95
horiK <hazemi94basic@gmail.com>
2020-05-05 21:09:38 +0900

    サウンドコンポーネント実装

---
+++ Walker/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource bgmAudioSource = null;
+
+    [SerializeField]
+    private AudioSource seAudioSource = null;
+
+    [SerializeField]
+    private AudioClip[] bgmAudioClips = null;
+
+    [SerializeField]
+    private AudioClip[] seAudioClips = null;
+
+    public enum eBGMAudioClip
+    {
+        BURNING_CAVERN = 0,
+    }
+
+    public enum eSEAudioClip
+    {
+        WHISTLE = 0,
+    }
+
+    public void PlayBGM(eBGMAudioClip _audioClip)
+    {
+        bgmAudioSource.clip = bgmAudioClips[(int)_audioClip];
+        bgmAudioSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmAudioSource.Stop();
+    }
+
+    public void PlayOneShotSE(eSEAudioClip _audioClip)
+    {
+        seAudioSource.PlayOneShot(seAudioClips[(int)_audioClip]);
+    }
+
+
+    public void StopSE()
+    {
+        seAudioSource.Stop();
+    }
+}
