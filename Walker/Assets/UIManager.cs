c016289b5bb4d073009c0c01ff17db40ad28f1cc
horiK <hazemi94basic@gmail.com>
2020-05-05 23:09:10 +0900

    階数表示するコンポーネント追加

---
+++ Walker/Assets/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    Text floorText = null;
+
+    public void SetFloor(int _floor)
+    {
+        floorText.text = _floor + "F";
+    }
+}
